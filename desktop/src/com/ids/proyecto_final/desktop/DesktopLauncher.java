1466bdc0d9ac837e01500f1ade9d006b5db56600
Kenneth Quintero <kennethgqv@gmail.com>
2021-09-30 16:52:53 -0600

    First commit

---
+++ desktop/src/com/ids/proyecto_final/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ids.proyecto_final.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ids.proyecto_final.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
