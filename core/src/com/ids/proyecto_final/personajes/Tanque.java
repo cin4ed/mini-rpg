685cc528736c912682eeeadfb1e3efc1975b6cf2
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:10:22 -0600

    Add Tanque Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Tanque.java
@@ -0,0 +1,10 @@
+package com.ids.proyecto_final.personajes;
+
+public class Tanque extends Personaje{
+    public Tanque() {
+        super("Tanque",500,25,5,38);
+    }
+
+    public void aumentarResistencia() {
+    }
+}
