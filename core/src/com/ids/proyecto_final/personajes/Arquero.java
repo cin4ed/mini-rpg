21db0ff52fb65c5c0fbcaa2e834ef184a1e6ab3c
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:34:05 -0600

    Add Arquero Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Arquero.java
@@ -0,0 +1,13 @@
+package com.ids.proyecto_final.personajes;
+
+public class Arquero extends Personaje{
+
+    public Arquero() {
+        super("Arquero",150,60,5,20);
+    }
+
+    public void ignorarArmadura() {
+    }
+    public void pasivaEvasion(){
+    }
+}
