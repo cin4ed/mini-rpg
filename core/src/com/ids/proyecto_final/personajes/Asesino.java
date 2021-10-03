bf477d8a9473f542683612db40075652b860e9ca
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:26:09 -0600

    Add Asesino Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Asesino.java
@@ -0,0 +1,11 @@
+package com.ids.proyecto_final.personajes;
+
+public class Asesino extends Personaje{
+
+    public Asesino() {
+        super("Asesino",250,55,3,15);
+    }
+
+    public void ejecutar() {
+    }
+}
