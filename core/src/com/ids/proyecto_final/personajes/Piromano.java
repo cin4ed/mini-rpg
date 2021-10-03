bf4c3b6cb825c96baf0131d19ca4c3bb2c5b648a
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:32:28 -0600

    Add Piromano Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Piromano.java
@@ -0,0 +1,11 @@
+package com.ids.proyecto_final.personajes;
+
+public class Piromano extends Personaje{
+
+    public Piromano() {
+        super("Piromano",230,30,5,28);
+    }
+
+    public void quemarTodo() {
+    }
+}
