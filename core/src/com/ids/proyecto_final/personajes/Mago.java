d5221a413f7609593a696c8129eadf3a3ca9ff90
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:18:54 -0600

    Add Mago Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Mago.java
@@ -0,0 +1,11 @@
+package com.ids.proyecto_final.personajes;
+
+public class Mago extends Personaje{
+
+    public Mago() {
+        super("Mago",350,40,5,25);
+    }
+
+    public void explosion() {
+    }
+}
