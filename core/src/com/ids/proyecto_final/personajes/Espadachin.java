3218f35ae3fde1c6531049cb13d5fb4179bfcae9
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-02 23:03:11 -0600

    Add Espadachin Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Espadachin.java
@@ -0,0 +1,11 @@
+package com.ids.proyecto_final.personajes;
+
+public class Espadachin extends Personaje {
+
+    public Espadachin() {
+        super("Espadachín",300,40,5,20);
+    }
+
+    public void toqueVenenoso() {
+    }
+}
