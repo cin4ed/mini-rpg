f11ee0445d2fc06269f38606543612aa69be4d87
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:16:34 -0600

    Add Pistolero Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Pistolero.java
@@ -0,0 +1,12 @@
+package com.ids.proyecto_final.personajes;
+
+public class Pistolero extends Personaje {
+
+    public Pistolero() {
+        super("Pistolero",140,45,5,20);
+    }
+
+    public void toqueVenenoso() {
+    }
+
+}
