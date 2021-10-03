db3be116bccd8dc151aaefc3b4fea26cfca8a36f
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-03 12:29:33 -0600

    Add Curador Subclass of Personaje

---
+++ core/src/com/ids/proyecto_final/personajes/Curador.java
@@ -0,0 +1,11 @@
+package com.ids.proyecto_final.personajes;
+
+public class Curador extends Personaje{
+
+    public Curador() {
+        super("Curador",300,40,10,25);
+    }
+
+    public void curarAliado() {
+    }
+}
